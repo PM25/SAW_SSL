4e3ce82e38bac031d80683a60507886615f2b6c1
PY <pyhuang97@gmail.com>
2022-11-06 21:28:59 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-python -m install torch==1.1.0 -y --no-cache-dir
-python -m install torchvision -y --no-cache-dir
-python -m install scipy -y --no-cache-dir
-python -m install matplotlib -y --no-cache-dir
-python -m install progress -y --no-cache-dir
+python -m pip install torch==1.1.0 -y --no-cache-dir
+python -m pip install torchvision -y --no-cache-dir
+python -m pip install scipy -y --no-cache-dir
+python -m pip install matplotlib -y --no-cache-dir
+python -m pip install progress -y --no-cache-dir
 python -m pip install git+https://github.com/ildoonet/pytorch-randaugment -y --no-cache-dir
 conda install pytorch-cuda=11.6 -c pytorch -c nvidia -y
