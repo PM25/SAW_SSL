5cbf98ca09fba778e36644e3a13378a84bb3b93e
PY <pyhuang97@gmail.com>
2022-11-07 00:58:56 +0800

    upload requirements.txt

--- install.sh
+++ install.sh
@@ -1,7 +1,1 @@
-python -m pip install torch==1.1.0 --no-cache-dir
-python -m pip install torchvision --no-cache-dir
-python -m pip install scipy --no-cache-dir
-python -m pip install matplotlib --no-cache-dir
-python -m pip install progress --no-cache-dir
 python -m pip install git+https://github.com/ildoonet/pytorch-randaugment --no-cache-dir
-conda install pytorch-cuda=11.6 -c pytorch -c nvidia
