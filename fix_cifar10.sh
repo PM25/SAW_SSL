726a472996c5ef51c7783819dc947c8c50538014
PY <pyhuang97@gmail.com>
2022-11-08 18:57:55 +0800

    add fixmatch configs.

---
+++ fix_cifar10.sh
@@ -0,0 +1,11 @@
+./fix_cifar10_l100_u0.01_1.sh
+./fix_cifar10_l100_u0.01_2.sh
+./fix_cifar10_l100_u0.01_3.sh
+
+./fix_cifar10_l100_u1_1.sh
+./fix_cifar10_l100_u1_2.sh
+./fix_cifar10_l100_u1_3.sh
+
+./fix_cifar10_l100_u100_1.sh
+./fix_cifar10_l100_u100_2.sh
+./fix_cifar10_l100_u100_3.sh
--- remix_cifar10.sh
+++ remix_cifar10.sh
@@ -1,11 +1,11 @@
-./remix_l100_u0.01_0.sh
 ./remix_l100_u0.01_1.sh
 ./remix_l100_u0.01_2.sh
+./remix_l100_u0.01_3.sh
 
-./remix_l100_u1_0.sh
 ./remix_l100_u1_1.sh
 ./remix_l100_u1_2.sh
+./remix_l100_u1_3.sh
 
-./remix_l100_u100_0.sh
 ./remix_l100_u100_1.sh
 ./remix_l100_u100_2.sh
+./remix_l100_u100_3.sh
