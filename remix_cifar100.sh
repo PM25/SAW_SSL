ddf633bfc2348e9aeda2f9ac04659b707d1621f7
PY <pyhuang97@gmail.com>
2022-11-08 09:26:05 +0800

    add cifar100 configs.

---
+++ remix_cifar100.sh
@@ -0,0 +1,7 @@
+./remix_cifar100_l10_u0.1_1.sh
+./remix_cifar100_l10_u0.1_2.sh
+./remix_cifar100_l10_u0.1_3.sh
+
+./remix_cifar100_l10_u10_1.sh
+./remix_cifar100_l10_u10_2.sh
+./remix_cifar100_l10_u10_3.sh
