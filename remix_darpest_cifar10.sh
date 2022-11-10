7542a9216e9959a4cb5f059bfec56be769eb37e8
PY <pyhuang97@gmail.com>
2022-11-10 09:12:25 +0800

    add remix darp estim cifar10.

---
+++ remix_darpest_cifar10.sh
@@ -0,0 +1,11 @@
+./remix_darpest_l100_u0.01_1.sh
+./remix_darpest_l100_u0.01_2.sh
+./remix_darpest_l100_u0.01_3.sh
+
+./remix_darpest_l100_u1_1.sh
+./remix_darpest_l100_u1_2.sh
+./remix_darpest_l100_u1_3.sh
+
+./remix_darpest_l100_u100_1.sh
+./remix_darpest_l100_u100_2.sh
+./remix_darpest_l100_u100_3.sh
